0a636347f9194dee2ca15f3160f88324e52e752c
mohammed talib <m.t.talib@hotmail.com>
2022-05-10 12:26:22 -0700

    initial commit

---
+++ la6.py
@@ -0,0 +1,37 @@
+"""
+
+To start we generate a random integer between 1 and 20, and based on the resuelt of the random code, we check to see if it falls
+under certain range
+if the number is greater than 35 then the results will be "cherries"
+otherwise if the number is > 10, then the result will be orange
+other wise if the number is >5 , then the results will be plum,
+otherwise if the number is > 2 then the result will be "Melon"
+otherwise if the number is > 1, then the result will be "bell"
+if the number is not any of the above, then the results will be "bar:
+
+we iterate over using a loop three times and print the results to the user, as an example plum cherries"
+
+"""
+
+"""
+
+num = generate random number
+
+if num is greater than 15,
+    then the result will be cherries
+otherwise if num is > 10 
+    then the results will be "orange"
+otherwise if num >5
+    then the results will be "Plum"
+otherwise if num is > 2 
+    then the results will be "Melon"
+otherwise if num > 1
+    then the results will be "Bell"
+otherwise the number is not of any of the above then the results will be "bar"
+
+we iterate over using a loop three times and print the result to the user as an example "plum cherries"
+
+
+
+
+"""
